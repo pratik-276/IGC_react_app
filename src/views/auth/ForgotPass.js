719c0551c645f5e03e06b8abe9e99d2e28609496
Harmis <harmis@Harmiss-Mac-mini-2.local>
2024-04-25 19:38:42 +0530

    initial changes

--- src/views/auth/ForgotPass.js
+++ src/views/auth/ForgotPass.js
@@ -3,26 +3,126 @@ import "./Auth.css";
 import { Link, useNavigate } from "react-router-dom";
 import OTPInput from "otp-input-react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import { FaEyeSlash } from "react-icons/fa";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const ForgotPass = () => {
   const navigate = useNavigate();
   const [show, setShow] = useState(false);
   const [resetShow, setResetShow] = useState(false);
 
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const [otp, setOtp] = useState("");
+  const [otpError, setOtpError] = useState("");
+
+  const [input, setInput] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [passwordType, setPasswordType] = useState("password");
+  const [confirmpasswordType, setConfirmPasswordType] = useState("password");
+
+  const validateEmail = () => {
+    if (!email.trim()) {
+      setEmailError("Email field is required");
+      return false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      setEmailError("Email field is invalid");
+      return false;
+    }
+    return true;
+  };
+
+  const validatePassword = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (!input["password"]) {
+      isValid = false;
+      errors["password"] = "Please enter your password.";
+    }
+
+    if (!input["confirmPassword"]) {
+      isValid = false;
+      errors["confirmPassword"] = "Please enter your confirm password.";
+    }
+
+    if (input["password"] && input["password"].length < 6) {
+      isValid = false;
+      errors["password"] = "Please add at least 6 characters.";
+    } else {
+      if (
+        input["password"] &&
+        input["confirmPassword"] &&
+        input["password"] !== input["confirmPassword"]
+      ) {
+        isValid = false;
+        errors["confirmPassword"] =
+          "Password and Confirm password doesn't match.";
+      }
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+    setEmailError("");
+  };
+
+  const handlePasswordChange = (event) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+    setErrors({});
+  };
+
+  const handleSubmitOtp = (e) => {
     e.preventDefault();
-    setShow(true);
+    if (validateEmail()) {
+      console.log("Email is valid:", email);
+      setShow(true);
+    }
   };
 
   const handleVerifyOtp = (e) => {
     e.preventDefault();
-    setResetShow(true);
+    const isValidOTP = otp.length === 5;
+    if (isValidOTP) {
+      setResetShow(true);
+    } else if (!isValidOTP) {
+      setOtpError("Please Enter Valid OTP");
+    }
   };
 
   const handleVerifyPassWord = (e) => {
     e.preventDefault();
-    navigate("/signup");
+    if (validatePassword()) {
+      navigate("/signup");
+    }
+  };
+
+  const togglePassword = (e) => {
+    e.preventDefault();
+    if (passwordType === "password") {
+      setPasswordType("text");
+      return;
+    }
+    setPasswordType("password");
+  };
+
+  const toggleConfirmPassword = (e) => {
+    e.preventDefault();
+    if (confirmpasswordType === "password") {
+      setConfirmPasswordType("text");
+      return;
+    }
+    setConfirmPasswordType("password");
   };
 
   return (
@@ -45,13 +145,25 @@ const ForgotPass = () => {
                           </p>
                         </div>
                         <div className="login_detail_inr p-0">
-                          <form onSubmit={handleSubmit}>
-                            <div className="form-group mb-4">
+                          <form onSubmit={handleSubmitOtp}>
+                            <div className="form-group mb-4 position-relative">
                               <input
                                 type="email"
-                                className="form-control"
+                                className={`form-control ${
+                                  emailError ? "is-invalid" : ""
+                                }`}
                                 placeholder="Email ID"
+                                value={email}
+                                onChange={handleChangeEmail}
                               />
+                              {emailError && (
+                                <div
+                                  className="invalid-feedback  mb-1"
+                                  style={{ fontSize: "14px" }}
+                                >
+                                  {emailError}
+                                </div>
+                              )}
                             </div>
 
                             <div className="forget_pass_btn">
@@ -63,8 +175,8 @@ const ForgotPass = () => {
                                   Back to sign in
                                 </button>
                               </div>
-                              <div className="auth_login_btn">
-                                <button className="forgetpass_btn_otp">
+                              <div className="auth_login_btn ">
+                                <button className="forgetpass_btn_otp forget_pass_send_otp_btn">
                                   Send OTP
                                 </button>
                               </div>
@@ -90,13 +202,28 @@ const ForgotPass = () => {
                               <form onSubmit={handleVerifyOtp}>
                                 <div className="otp_input_box mb-3">
                                   <OTPInput
-                                    className="otp-container"
+                                    className={`otp-container ${
+                                      otpError ? "is-invalid" : ""
+                                    }`}
                                     autoFocus
                                     OTPLength={5}
                                     otpType="number"
                                     disabled={false}
                                     placeholder="000000"
+                                    value={otp}
+                                    onChange={(otp) => {
+                                      setOtp(otp);
+                                      setOtpError("");
+                                    }}
                                   />
+                                  {otpError && (
+                                    <div
+                                      className="invalid-feedback "
+                                      style={{ fontSize: "14px" }}
+                                    >
+                                      {otpError}
+                                    </div>
+                                  )}
                                 </div>
 
                                 <div className="mb-4">
@@ -117,7 +244,7 @@ const ForgotPass = () => {
                                     </button>
                                   </div>
                                   <div className="auth_login_btn">
-                                    <button className="forgetpass_btn_otp">
+                                    <button className="forgetpass_btn_otp forget_pass_send_otp_btn">
                                       Reset Password
                                     </button>
                                   </div>
@@ -141,25 +268,66 @@ const ForgotPass = () => {
                         </div>
                         <div className="login_detail_inr p-0 reset_pass_details">
                           <form onSubmit={handleVerifyPassWord}>
-                            <div className="form-group mb-4 position-relative ">
-                              <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Enter Password"
-                              />
-                              <div className="show_pass_first_eye">
-                                <FaEyeSlash />
+                            <div className="form-group mb-3 position-relative">
+                              <div className="position-relative">
+                                <input
+                                  type={passwordType}
+                                  className="form-control"
+                                  name="password"
+                                  placeholder="Enter Password"
+                                  value={input.password}
+                                  onChange={handlePasswordChange}
+                                />
+
+                                <div
+                                  className="show_pass_first_eye"
+                                  onClick={(e) => togglePassword(e)}
+                                >
+                                  {passwordType === "password" ? (
+                                    <FaEye />
+                                  ) : (
+                                    <FaEyeSlash />
+                                  )}
+                                </div>
                               </div>
+                              {errors.password && (
+                                <div
+                                  className="text-danger m-0 pb-1"
+                                  style={{ fontSize: "14px" }}
+                                >
+                                  {errors.password}
+                                </div>
+                              )}
                             </div>
-                            <div className="form-group mb-4 position-relative">
-                              <input
-                                type="email"
-                                className="form-control"
-                                placeholder="Confirm Password"
-                              />
-                              <div className="show_pass_first_eye">
-                                <FaEyeSlash />
+                            <div className="form-group mb-3 position-relative">
+                              <div className="position-relative">
+                                <input
+                                  type={confirmpasswordType}
+                                  name="confirmPassword"
+                                  className="form-control"
+                                  placeholder="Confirm Password"
+                                  value={input.confirmPassword}
+                                  onChange={handlePasswordChange}
+                                />
+                                <div
+                                  className="show_pass_first_eye"
+                                  onClick={(e) => toggleConfirmPassword(e)}
+                                >
+                                  {confirmpasswordType === "password" ? (
+                                    <FaEye />
+                                  ) : (
+                                    <FaEyeSlash />
+                                  )}
+                                </div>
                               </div>
+                              {errors.confirmPassword && (
+                                <div
+                                  className="text-danger m-0 pb-2"
+                                  style={{ fontSize: "14px" }}
+                                >
+                                  {errors.confirmPassword}
+                                </div>
+                              )}
                             </div>
 
                             <div className="create_pass_btns">
--- src/views/auth/Signup.js
+++ src/views/auth/Signup.js
@@ -1,10 +1,121 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Auth.css";
 import { FcGoogle } from "react-icons/fc";
 import { IoIosArrowForward } from "react-icons/io";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [passShow, setPassShow] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [input, setInput] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [passwordType, setPasswordType] = useState("password");
+  const [confirmpasswordType, setConfirmPasswordType] = useState("password");
+
+  const validatePassword = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (!input["password"]) {
+      isValid = false;
+      errors["password"] = "Please enter your password.";
+    }
+
+    if (!input["confirmPassword"]) {
+      isValid = false;
+      errors["confirmPassword"] = "Please enter your confirm password.";
+    }
+
+    if (input["password"] && input["password"].length < 6) {
+      isValid = false;
+      errors["password"] = "Please add at least 6 characters.";
+    } else {
+      if (
+        input["password"] &&
+        input["confirmPassword"] &&
+        input["password"] !== input["confirmPassword"]
+      ) {
+        isValid = false;
+        errors["confirmPassword"] =
+          "Password and Confirm password doesn't match.";
+      }
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setIsValidEmail(true);
+    setErrorMessage("");
+  };
+
+  const handlePasswordChange = (event) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+    setErrors({});
+  };
+
+  const validateEmail = (email) => {
+    return email.includes("@") && email.includes(".");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email.trim() === "") {
+      setIsValidEmail(false);
+      setErrorMessage("Please enter your email");
+      return;
+    }
+    const isValid = validateEmail(email);
+    setIsValidEmail(isValid);
+    if (isValid) {
+      console.log("Valid email:", email);
+      setPassShow(true);
+    } else {
+      setErrorMessage("Please enter a valid email address");
+      console.log("Invalid email:", email);
+    }
+  };
+
+  const handleVerifyPassWord = (e) => {
+    e.preventDefault();
+    if (validatePassword()) {
+      navigate("/login");
+    }
+  };
+
+  const togglePassword = (e) => {
+    e.preventDefault();
+    if (passwordType === "password") {
+      setPasswordType("text");
+      return;
+    }
+    setPasswordType("password");
+  };
+
+  const toggleConfirmPassword = (e) => {
+    e.preventDefault();
+    if (confirmpasswordType === "password") {
+      setConfirmPasswordType("text");
+      return;
+    }
+    setConfirmPasswordType("password");
+  };
+
   return (
     <>
       <div className="auth_main">
@@ -17,40 +128,127 @@ const Signup = () => {
                   <div className="signup_detail">
                     <div className="signup_title">
                       <h3>Sign Up</h3>
-                      <p className="pb-4">Your partner in gaming insights!</p>
+                      <p className="pb-3">Your partner in gaming insights!</p>
                       <span>Lets get you started...</span>
                     </div>
-                    <div className="login_detail_inr signup_details_inr p-0">
-                      <form>
-                        <div className="form-group position-relative">
-                            <label>Email</label>
-                          <input
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                          />
-                        </div>
-                        <div className="auth_login_btn mb-4">
-                          <button className="login_btn">
-                            Next
-                            <IoIosArrowForward className="ms-2" />
-                          </button>
-                        </div>
-                        <h4 className="pb-2">
-                          <span>Or Sign in with</span>
-                        </h4>
-                        <button className="btn google_login">
-                          <FcGoogle className="me-2 google_icon" />
-                          Sign in with Google
-                        </button>
+                    <div className="login_detail_inr signup_details_inr signup_page_input p-0">
+                      {passShow === false && (
+                        <>
+                          <form onSubmit={handleSubmit}>
+                            <div className="form-group position-relative ">
+                              <label>Email</label>
+                              <input
+                                type="email"
+                                className={`form-control ${
+                                  !isValidEmail && "is-invalid"
+                                }`}
+                                placeholder="Email ID"
+                                value={email}
+                                onChange={handleEmailChange}
+                              />
+                              {!isValidEmail && (
+                                <div className="invalid-feedback position-absolute m-0">
+                                  {errorMessage}
+                                </div>
+                              )}
+                            </div>
+                            <div className="auth_login_btn mb-4">
+                              <button className="login_btn">
+                                Next
+                                <IoIosArrowForward className="ms-2" />
+                              </button>
+                            </div>
+                          </form>
+                        </>
+                      )}
+                      {passShow === true && (
+                        <>
+                          <form onSubmit={handleVerifyPassWord}>
+                            <div className="form-group position-relative">
+                              <label>password</label>
+                              <div className="position-relative">
+                                <input
+                                  type={passwordType}
+                                  name="password"
+                                  className="form-control"
+                                  placeholder="Enter Password"
+                                  value={input.password}
+                                  onChange={handlePasswordChange}
+                                />
+                                <div
+                                  className="show_pass_first_eye"
+                                  onClick={(e) => togglePassword(e)}
+                                >
+                                  {passwordType === "password" ? (
+                                    <FaEye />
+                                  ) : (
+                                    <FaEyeSlash />
+                                  )}
+                                </div>
+                              </div>
+                              {errors.password && (
+                                <div
+                                  className="text-danger mb-2"
+                                  style={{ fontSize: "14px" }}
+                                >
+                                  {errors.password}
+                                </div>
+                              )}
+                            </div>
+                            <div className="form-group position-relative">
+                              <label>Confirm Password</label>
+                              <div className="position-relative">
+                                <input
+                                  type={confirmpasswordType}
+                                  name="confirmPassword"
+                                  className="form-control"
+                                  placeholder="Confirm Password"
+                                  value={input.confirmPassword}
+                                  onChange={handlePasswordChange}
+                                />
+                                <div
+                                  className="show_pass_first_eye"
+                                  onClick={(e) => toggleConfirmPassword(e)}
+                                >
+                                  {confirmpasswordType === "password" ? (
+                                    <FaEye />
+                                  ) : (
+                                    <FaEyeSlash />
+                                  )}
+                                </div>
+                              </div>
+                              {errors.confirmPassword && (
+                                <div
+                                  className="text-danger mb-2"
+                                  style={{ fontSize: "14px" }}
+                                >
+                                  {errors.confirmPassword}
+                                </div>
+                              )}
+                            </div>
+                            <div className="auth_login_btn mb-4">
+                              <button className="login_btn">
+                                Next
+                                <IoIosArrowForward className="ms-2" />
+                              </button>
+                            </div>
+                          </form>
+                        </>
+                      )}
+                      <h4 className="pb-2">
+                        <span>Or Sign in with</span>
+                      </h4>
+                      <button className="btn google_login">
+                        <FcGoogle className="me-2 google_icon" />
+                        Sign in with Google
+                      </button>
 
-                        <div className="auth_signup_link mt-5">
-                          <p className="m-0">Already have account?</p>
-                          <Link to="/login" className="ms-2">
-                            Login here
-                          </Link>
-                        </div>
-                      </form>
+                      <div className="auth_signup_link mt-5">
+                        <p className="m-0">Already have account?</p>
+                        <Link to="/login" className="ms-2">
+                          Login here
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
