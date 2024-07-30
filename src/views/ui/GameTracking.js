0e434380bfe48181a81dd8b7305e8e000866e883
Harmis <harmis@Harmiss-Mac-mini-2.local>
2024-07-30 19:21:43 +0530

    new changeas

--- src/views/ui/GameTracking.js
+++ src/views/ui/GameTracking.js
@@ -1,12 +1,46 @@
 import React from "react";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+
+const TrackingTime = ["7days", "1 month", "3 months", "custom"];
 
 const GameTracking = () => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h4>Hello Game tracking</h4>
+      <div className="compass">
+        <div className="compass-data">
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <h4>Overview Dashboard</h4>
+              <span>
+                View, Filter and analyse data as per your requirements with
+                adaptive dashboard
+              </span>
+            </div>
+
+            <div className="col-md-6">
+              <div className="row justify-content-end">
+                <div className="col-md-3">
+                  <Dropdown
+                    options={TrackingTime}
+                    placeholder="Status"
+                    className="w-100"
+                  />
+                </div>
+                <div className="col-md-3">
+                  <Dropdown
+                    options={TrackingTime}
+                    placeholder="All time "
+                    className="w-100"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="tracker-details">
+          <div className="tracker-details-head">
+            <h5>Tracker Details</h5>
           </div>
         </div>
       </div>
