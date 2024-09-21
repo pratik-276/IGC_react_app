97e34364cd41837939a07061c9defcc7ec0db98e
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2024-09-21 16:05:11 +0530

    video added to home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,16 @@
+import Video from "../assets/images/intro.mp4"
+
 const Home = () => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <h4>Hello Home</h4>
+      <div className="container h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-12 d-flex flex-column justify-content-center align-items-center">
+            <video src={Video} autoPlay="true" style={{
+              width: "800px",
+              marginBottom: "50px"
+            }} />
+            <h4>Reports coming soon ...</h4>
           </div>
         </div>
       </div>
