9dce022f0448c2df27124503da94632bb7140c02
Akshay Patil <akshay.patil@geanysoftech.com>
2025-04-03 19:16:11 +0530

    Daily Sectional Analytics updated v2

--- src/charts/DailySectionalAnalyticshart.jsx
+++ src/charts/DailySectionalAnalyticshart.jsx
@@ -40,33 +40,33 @@ const DailySectionalAnalyticsChart = ({ trackingDetails }) => {
             <h5 className="font-semibold pl-2">Daily Sectional Analytics</h5>
             <div className="mt-2 " style={{ overflowX: 'auto', maxHeight: '400px' }}>
                 <table style={{ overflowX: 'auto', overflowY: 'auto', maxWidth: '100%' }}>
-                    <thead>
+                    <thead style={{ position: 'sticky', top: 0, background: '#fff', zIndex: 2, borderBottom: '2px solid #392f6c' }} >
                         <tr>
                             <th style={{ padding: '8px', border: '1px solid #392f6c', textAlign: 'center' }}>Section Name</th>
                             <th style={{ padding: '8px', border: '1px solid #392f6c', textAlign: 'center' }}>Section Position</th>
                             {dates.map(date => (
-                                <th key={date} style={{ padding: '8px', border: '1px solid #392f6c', textAlign: 'center' }}>
+                                <th key={date} style={{ padding: '8px', border: '1px solid #392f6c', textAlign: 'center', minWidth: '80px' }}>
                                     {format(new Date(date), 'MMM dd, yyyy')}
                                 </th>
                             ))}
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody >
                         {tableData.map((row, index) => (
                             <tr key={index}>
                                 {row.section_name !== null ? (
                                     <td
                                         rowSpan={Object.keys(groupedData[row.section_name]).length}
-                                        style={{ padding: '8px', border: '1px solid #392f6c', textAlign: 'center' }}
+                                        style={{ padding: '4px', border: '1px solid #392f6c', textAlign: 'center' }}
                                     >
                                         {row.section_name}
                                     </td>
                                 ) : null}
-                                <td style={{ padding: '8px', border: '1px solid #392f6c', textAlign: 'center' }}>
+                                <td style={{ padding: '4px', border: '1px solid #392f6c', textAlign: 'center' }}>
                                     {row.section_position}
                                 </td>
                                 {dates.map(date => (
-                                    <td key={date} style={{ padding: '8px', border: '1px solid #392f6c', textAlign: 'center', backgroundColor: row[date] ? '#DAD2FF' : 'transparent' }}>
+                                    <td key={date} style={{ padding: '4px', border: '1px solid #392f6c', textAlign: 'center', backgroundColor: row[date] ? '#DAD2FF' : 'transparent' }}>
                                         {row[date] || ''}
                                     </td>
                                 ))}
