82eeba69cc564be9f63dbf3f50f47324594ffd3e
Akshay Patil <akshay.patil@geanysoftech.com>
2025-05-09 15:36:43 +0530

    game-details-mod loader fixed

--- src/views/ui/GameDetailsMod.js
+++ src/views/ui/GameDetailsMod.js
@@ -19,11 +19,10 @@ const GameDetailsMod = () => {
   const navigate = useNavigate();
   const user_id = localStorage.getItem("user_id");
   const [loader, setLoader] = useState(true);
-  const [trackingDetails, setTrackingDetails] = useState([]);
+  const [trackingDetails, setTrackingDetails] = useState(null);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
 
-  const [maxDate, setMaxDate] = useState(null);
   const [minDate, setMinDate] = useState(null);
 
   const location = useLocation();
@@ -31,27 +30,28 @@ const GameDetailsMod = () => {
   const { operator_site_id, game_name, casino_name, country_name, state_name } =
     location.state || {};
 
-  // Set the default values for start and end date
   useEffect(() => {
     const currentDate = new Date();
-
     const previousMonth = new Date(currentDate);
+
     previousMonth.setMonth(currentDate.getMonth() - 1);
     previousMonth.setDate(1);
 
     setMinDate(previousMonth);
-
     setStartDate(previousMonth);
     setEndDate(currentDate);
   }, []);
 
   useEffect(() => {
-    getDashboardData({
-      site_id: operator_site_id,
-      game_name: game_name,
-      start_date: startDate,
-      end_date: endDate,
-    });
+    if (startDate && endDate && operator_site_id && game_name) {
+      setLoader(true);
+      getDashboardData({
+        site_id: operator_site_id,
+        game_name: game_name,
+        start_date: startDate,
+        end_date: endDate,
+      });
+    }
   }, [user_id, startDate, endDate]);
 
   const getDashboardData = ({ site_id, game_name, start_date, end_date }) => {
@@ -68,14 +68,11 @@ const GameDetailsMod = () => {
       },
     })
       .then((res) => {
-        console.log("res : ", res);
-        console.log("res data : ", res.data);
         setTrackingDetails(res.data);
-        setLoader(false);
       })
       .catch((err) => {
         console.log("Error fetching data: ", err);
-        setLoader(false);
+        setTrackingDetails(null);
       })
       .finally(() => {
         setLoader(false);
@@ -93,7 +90,7 @@ const GameDetailsMod = () => {
                   fontSize: "30px",
                   cursor: "pointer",
                 }}
-                onClick={() => navigate("/dashboard-mod")} // Navigate to /dashboard-mod when clicked
+                onClick={() => navigate("/dashboard-mod")}
               />
               <div>
                 <h4
@@ -147,114 +144,104 @@ const GameDetailsMod = () => {
               </div>
             </div>
           </div>
+        ) : trackingDetails && trackingDetails.latest_position ? (
+          <>
+            <div className="mt-3">
+              <h5 className="font-semibold pl-2">Indicators</h5>
+              <div className="flex gap-2 mt-2">
+                <InfoCard
+                  header="Selected Game"
+                  tooltip="Shows selected game name"
+                  tooltipTarget="game_name"
+                  value={game_name}
+                />
+
+                <InfoCard
+                  header="Selected Casino"
+                  tooltip="Shows selected casino name"
+                  tooltipTarget="casino_name"
+                  value={casino_name}
+                />
+
+                <InfoCard
+                  header="Country"
+                  tooltip="Shows country name"
+                  tooltipTarget="country_name"
+                  value={country_name}
+                />
+
+                <InfoCard
+                  header="State"
+                  tooltip="Shows state name"
+                  tooltipTarget="state_name"
+                  value={state_name ? state_name : "-"}
+                />
+              </div>
+            </div>
+
+            <div className="border border-secondary p-3 rounded-3 mt-3">
+              {/* Tracker Details Table */}
+              <div>
+                <h5 className="font-semibold pl-2">Latest Details</h5>
+
+                <div className="flex gap-2 mt-2">
+                  <InfoCard
+                    header="Overall Position"
+                    tooltip="Shows state name"
+                    tooltipTarget="overall_game_nonvisible_position"
+                    value={
+                      trackingDetails.latest_position
+                        .overall_game_nonvisible_position
+                    }
+                  />
+                  <InfoCard
+                    header="Section Name"
+                    tooltip="Shows Section Name"
+                    tooltipTarget="section_name"
+                    value={trackingDetails.latest_position.section_name}
+                  />
+                  <InfoCard
+                    header="Sectional Position"
+                    tooltip="Shows Sectional Position"
+                    tooltipTarget="section_position"
+                    value={trackingDetails.latest_position.section_position}
+                  />
+                  <InfoCard
+                    header="Sectional Game Position"
+                    tooltip="Shows Sectional Game Position"
+                    tooltipTarget="game_position"
+                    value={trackingDetails.latest_position.game_position}
+                  />
+                  <InfoCard
+                    header="Latest Date"
+                    tooltip="Shows Latest Date"
+                    tooltipTarget="created_date"
+                    value={trackingDetails.latest_position.created_date}
+                  />
+                </div>
+
+                <div className="mt-4">
+                  <AveragePositionChart trackingDetails={trackingDetails} />
+                </div>
+              </div>
+            </div>
+
+            <div className="border border-secondary p-3 rounded-3 mt-3">
+              <div>
+                <DailySectionalAnalyticshart
+                  trackingDetails={trackingDetails}
+                />
+              </div>
+            </div>
+          </>
         ) : (
           <>
-            {trackingDetails &&
-            Object.keys(trackingDetails).length > 0 &&
-            trackingDetails.latest_position ? (
-              <>
-                <div className="mt-3">
-                  <h5 className="font-semibold pl-2">Indicators</h5>
-                  <div className="flex gap-2 mt-2">
-                    <InfoCard
-                      header="Selected Game"
-                      tooltip="Shows selected game name"
-                      tooltipTarget="game_name"
-                      value={game_name}
-                    />
-
-                    <InfoCard
-                      header="Selected Casino"
-                      tooltip="Shows selected casino name"
-                      tooltipTarget="casino_name"
-                      value={casino_name}
-                    />
-
-                    <InfoCard
-                      header="Country"
-                      tooltip="Shows country name"
-                      tooltipTarget="country_name"
-                      value={country_name}
-                    />
-
-                    <InfoCard
-                      header="State"
-                      tooltip="Shows state name"
-                      tooltipTarget="state_name"
-                      value={state_name ? state_name : "-"}
-                    />
-                  </div>
-                </div>
-
-                <div className="border border-secondary p-3 rounded-3 mt-3">
-                  {/* Tracker Details Table */}
-                  <div>
-                    <h5 className="font-semibold pl-2">Latest Details</h5>
-
-                    <div className="flex gap-2 mt-2">
-                      <InfoCard
-                        header="Overall Position"
-                        tooltip="Shows state name"
-                        tooltipTarget="overall_game_nonvisible_position"
-                        value={
-                          trackingDetails.latest_position
-                            .overall_game_nonvisible_position
-                        }
-                      />
-                      <InfoCard
-                        header="Section Name"
-                        tooltip="Shows Section Name"
-                        tooltipTarget="section_name"
-                        value={trackingDetails.latest_position.section_name}
-                      />
-                      <InfoCard
-                        header="Sectional Position"
-                        tooltip="Shows Sectional Position"
-                        tooltipTarget="section_position"
-                        value={trackingDetails.latest_position.section_position}
-                      />
-                      <InfoCard
-                        header="Sectional Game Position"
-                        tooltip="Shows Sectional Game Position"
-                        tooltipTarget="game_position"
-                        value={trackingDetails.latest_position.game_position}
-                      />
-                      <InfoCard
-                        header="Latest Date"
-                        tooltip="Shows Latest Date"
-                        tooltipTarget="created_date"
-                        value={trackingDetails.latest_position.created_date}
-                      />
-                    </div>
-
-                    <div className="mt-4">
-                      <AveragePositionChart trackingDetails={trackingDetails} />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="border border-secondary p-3 rounded-3 mt-3">
-                  <div>
-                    <DailySectionalAnalyticshart
-                      trackingDetails={trackingDetails}
-                    />
-                  </div>
-                </div>
-              </>
-            ) : (
-              <>
-                <div
-                  className="d-flex justify-content-center"
-                  style={{ marginTop: "15%" }}
-                >
-                  <h4>
-                    {trackingDetails
-                      ? "Tracking data is unavailable"
-                      : "No trackers configured"}
-                  </h4>
-                </div>
-              </>
-            )}
+            <div
+              className="d-flex justify-content-center"
+              style={{ marginTop: "15%" }}
+            >
+              <h4>Tracking data is unavailable</h4>
+            </div>
           </>
         )}
       </div>
